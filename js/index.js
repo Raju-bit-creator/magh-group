76d9638be03c4fd0b7fb33cd4c5302c6efacb7ca
Raju <rajutamang3456@gmail.com>
2025-02-16 16:00:31 +0545

    bootstrap

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// alert("are you sure to visit this page??");
+
+// let conf = confirm("are you okay?");
+// console.log(conf);
+
+// let prmt = prompt("which is your best fruit?");
+// console.log(prmt);
