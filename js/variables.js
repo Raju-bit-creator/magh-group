322f0b0c160ea9376559ccc656432aec7e61c094
Raju <rajutamang3456@gmail.com>
2025-02-27 15:58:14 +0545

    variables

---
+++ js/variables.js
@@ -0,0 +1,18 @@
+let a = 20;
+let b = 30;
+// console.log("the sum is :", a + b);
+
+// variable declaration
+// var, let and const
+// var c = 20;
+// var c = 30;
+// var c = 40;
+
+// console.log("value of c is:", c);
+
+let d = 20;
+let e = 40;
+console.log("value of d is: ", d);
+
+// const e = 30;
+// const e = 60;
