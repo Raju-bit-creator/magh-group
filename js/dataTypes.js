6af7a4b29e68ed9967e3a5ecd56e50f87f97481e
Raju <rajutamang3456@gmail.com>
2025-02-28 15:50:34 +0545

    datatypes

---
+++ js/dataTypes.js
@@ -0,0 +1,47 @@
+// Data Types
+// - String
+// Number  — integer, double, float
+// Boolean
+// null
+// undefined  // in js only
+// Object  {key:value}
+// Array collection of similar data types
+// {
+//     "place dfsfd"="pokhara",
+//     place = "kathmandu"
+
+// }
+let array = [1, 2, 3, 4, 5, 70, 100];
+(index = 0), 1, 2, 3, 4, 5, 6;
+let students = ["ram", "shyam", "hari", "bhola"];
+// console.log(array1); //undefined
+
+let student1 = [
+  {
+    name: "ram",
+    age: 20,
+    address: "kathmandu",
+    college: "cxdfdfd",
+  },
+  {
+    name: "shyam",
+    age: 18,
+    address: "Baudha",
+    college: "dfsdfds",
+  },
+  {
+    name: "Hari",
+    age: 10,
+    address: "kathmandu",
+    college: "cscsacsa",
+  },
+  {
+    name: "dfsdfsd",
+    age: 10,
+    address: "kathmandu",
+    college: "cscsacsa",
+  },
+];
+console.log("total object:", student1.length);
+console.log(students);
+console.log(array);
--- js/variables.js
+++ js/variables.js
@@ -1,5 +1,5 @@
-let a = 20;
-let b = 30;
+// let a = 20;
+// let b = 30;
 // console.log("the sum is :", a + b);
 
 // variable declaration
@@ -10,9 +10,23 @@ let b = 30;
 
 // console.log("value of c is:", c);
 
-let d = 20;
-let e = 40;
-console.log("value of d is: ", d);
+// let d = 20;
+// let e = 40;
+// console.log("value of d is: ", d);
 
-// const e = 30;
-// const e = 60;
+// const f = 30;
+// const g = 60;
+// let a = 30;
+
+// {
+//   let a = 40;
+//   console.log("value of first a:", a);
+// }
+// console.log("value of second a:", a);
+let a = 30;
+
+{
+  let a = 40;
+  console.log("value of first a:", a);
+}
+console.log("value of second a:", a);
