24998bbaff7681c93a8ab8d3be9fc0e16780e363
Raju <rajutamang3456@gmail.com>
2025-03-02 15:51:51 +0545

    conditions

---
+++ js/conditions.js
@@ -0,0 +1,40 @@
+let sun = true;
+let rain = false;
+let clouds = false;
+
+if (sun == false) {
+  console.log("It's not sunny outside");
+} else if (rain == true) {
+  console.log("please take umcrella");
+} else if (clouds) {
+  console.log("wear sweater or jackets");
+} else {
+  console.log("no need to have cold drinks");
+}
+
+let paidStatus = false;
+let scholaship = true;
+let student = true;
+if (scholaship == true) {
+  console.log("can give exam");
+} else {
+  console.log("can't give exam");
+}
+
+let a = 10;
+let d = 19;
+let c = 15;
+// check odd or even numcer
+if (c % 2 == 0) {
+  console.log(c + " is even numcer"); // + for concatination
+} else {
+  console.log(c + " is odd numcer");
+}
+
+let age = 19;
+let name = "John";
+if (age >= 18) {
+  console.log("you can vote");
+} else {
+  console.log("you cannot vote");
+}
