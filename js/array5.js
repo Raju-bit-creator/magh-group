fc024497ee93a162d585aab03d9a5a736640f242
Raju <rajutamang3456@gmail.com>
2025-03-14 15:44:39 +0545

    fix: masked Number

--- js/array5.js
+++ js/array5.js
@@ -36,7 +36,14 @@ users.forEach((user) => {
 });
 
 let users1 = [
-  "ram",
+  // {
+  //   name: "John",
+  //   age: 20,
+  //   class: 10,
+  //   password: "xxxxxx",
+  //   email: "jhon@gmail.com",
+  //   phone: "1234567890",
+  // },
   "shyam",
   "hari",
   "gopal",
@@ -50,3 +57,32 @@ let users1 = [
 
 //welcome back! you are register user
 //  else you are not register user so you are not allowed to access this page
+
+const registerUser = (name) => {
+  const user = users1.find((u) => {
+    return u == name;
+  });
+  if (user) {
+    console.log(`you are register user `);
+  } else {
+    console.log(`you are not register user `);
+  }
+};
+const findUser = registerUser("sita1");
+
+// assignment
+let phoneNumber = "1234567890";
+
+// output should be xxxxxxxx90
+
+// accout number masking
+
+const accountNumber = 344565677889;
+
+function maskAccountNumber(accountNumber) {
+  const numberToString = accountNumber.toString();
+  const lastFour = numberToString.slice(-4);
+  const maskedPart = "x".repeat(numberToString.length - 4);
+  return maskedPart + lastFour;
+}
+console.log(maskAccountNumber(accountNumber));
