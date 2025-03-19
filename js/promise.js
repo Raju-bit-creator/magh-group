d2f1cc6e8267c3453493a9c9addb63e76fd4441d
Raju <rajutamang3456@gmail.com>
2025-03-19 14:51:22 +0545

    async-await

--- js/promise.js
+++ js/promise.js
@@ -2,11 +2,11 @@
 //promise
 
 //  normal function
-function multiply(x, y) {
-  return x * y;
-}
-const result = multiply(5, 6);
-console.log(result);
+// function multiply(x, y) {
+//   return x * y;
+// }
+// const result = multiply(5, 6);
+// console.log(result);
 
 //arrow function
 // const multiply = (x, y) => {
@@ -15,28 +15,59 @@ console.log(result);
 // const result1 = multiply(5, 6);
 // console.log(result1);
 
-const car = (brand) => {
-  setTimeout(() => {
-    console.log(brand);
-  }, 1000);
-  return brand;
-};
-const result2 = car("tesla");
-console.log(result2);
+// const car = (brand) => {
+//   setTimeout(() => {
+//     console.log(brand);
+//   }, 1000);
+//   return brand;
+// };
+// const result2 = car("tesla");
+// console.log(result2);
 
 ///////////promise /////////
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // reject("internal server error  ||  error on line 23");
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // reject("internal server error  ||  error on line 23");
 
-    resolve({
-      data: "data from promise",
-    });
-  }, 2000);
-});
+//     resolve({
+//       data: "data from promise",
+//     });
+//   }, 2000);
+// });
 
-promise
-  .then((data) => console.log(data))
-  .catch((error) => {
-    console.log(error);
-  });
+// promise
+//   .then((data) => console.log(data))
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     let success = false;
+//     if (success) {
+//       resolve("data from promise");
+//     } else {
+//       reject("error on line 43");
+//     }
+//   }, 2000);
+// });
+// myPromise.then((m) => console.log(m)).catch((error) => console.log(error));
+
+// async and await
+// const fetchData = async () => {
+//   let response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   let data = response.json();
+//   console.log("this is response", data);
+// };
+// fetchData();
+
+const fetchData = async () => {
+  try {
+    let response = await fetch("https://jsonplaceholder.typicode.com/todos/3");
+    let data = response.json();
+    console.log("this is response", data);
+  } catch (error) {
+    throw new Error("internal server error");
+  }
+};
+fetchData();
